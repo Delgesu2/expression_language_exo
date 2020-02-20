e93a0adf59c6f8fb8625d6a0dd601df4dcc64e10
delgesu2 <xavierus70@hotmail.com>
2020-02-20 18:07:10 +0100

    Class for discount and command class

--- src/Command/DiscountCommand.php
+++ src/Command/DiscountCommand.php
@@ -1,15 +1,56 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ronsard
- * Date: 19/02/20
- * Time: 21:02
- */
 
 namespace App\Command;
 
+use App\Service\Discount;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-class DiscountCommand
+class DiscountCommand extends Command
 {
+    /**
+     * @var Discount
+     */
+    private $discount;
 
+    protected static $defaultName = 'app:start-discount';
+
+    /**
+     * DiscountCommand constructor.
+     * @param Discount $discount
+     */
+    public function __construct(Discount $discount)
+    {
+        parent::__construct();
+        $this->discount = $discount;
+    }
+
+    protected function configure()
+    {
+        $this
+
+            // command name (after "bin/console")
+            ->setName('app:start-discount')
+
+           // shown while running "php bin/console list"
+            ->$this->setDescription('Apply discount on articles')
+
+           // full description when running the command with "--help" option
+            ->$this->setHelp('Cette commande applique toutes les expressions en BDD à la liste des produits disponibles.')
+
+            ;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return int|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->discount->changeProductPrice();
+
+    }
 }
--- tests/UnitTest/DiscountTest.php
+++ tests/UnitTest/DiscountTest.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ronsard
- * Date: 19/02/20
- * Time: 15:24
- */
 
 namespace App\Tests\UnitTest;
 
@@ -14,13 +8,18 @@ use App\Service\Discount;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
 
-
+/**
+ * Class DiscountTest
+ *
+ * @package App\Tests\UnitTest
+ */
 class DiscountTest extends TestCase
 {
     public function testChangeProductPrice()
     {
-        $product = new Product();
-        $rule    = new Rules();
+        $product  = new Product();
+        $rule     = new Rules();
+        $discount = new Discount();
 
         $product->setPrice('8');
         $product->setType('ménage');
@@ -28,6 +27,10 @@ class DiscountTest extends TestCase
 
         $rule->setDiscountPercent(30);
         $rule->setRuleExpression('product.type=\'ménage\' and product.price > 5');
+
+        $discount->changeProductPrice();
+
+        $this->assertEquals(2.4, $product->getDiscountedPrice(),'L\'expression fonctionne');
 
     }
 }
